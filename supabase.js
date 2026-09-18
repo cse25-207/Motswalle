50b6e6e36de64cd4af8206eef01a932281372594
cse25-207 <cse25-207@thuto.bac.ac.bw>
2026-09-18 23:22:50 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://lxxzgkbdambsubktqrzj.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_-3i2q8aCbScEDWHDLAuvBg_eCilVreL";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
